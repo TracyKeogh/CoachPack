addc1df98020fbef3cb35bb081c29fa93d26720e
TracyKeogh <tracykeogh0@gmail.com>
2025-08-31 22:42:46 +0100

    Fix unterminated template literal in VisionBoard

--- src/components/VisionBoard.tsx
+++ src/components/VisionBoard.tsx
@@ -217,7 +217,7 @@ const VisionBoard: React.FC = () => {
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
-    a.download = \`vision-board-${new Date().toISOString().split('T')[0]}.json`;
+    a.download = `vision-board-${new Date().toISOString().split('T')[0]}.json`;
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
