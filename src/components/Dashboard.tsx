9059d49bbd6e1198d4958e1076221822cf206446
TracyKeogh <tracykeogh0@gmail.com>
2025-09-02 09:10:59 +0100

    Replace lucide-react icons with Unicode symbols in Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
 import { useDashboardData } from '../hooks/useDashboardData';
 import { useGoalSettingData } from '../hooks/useGoalSettingData';
 
